2ba08af9a90d0cd78dba9687c2e91face00232c2
jfrkin <119818044+jfrkin@users.noreply.github.com>
2022-12-04 23:07:06 +0100

    log serving web page

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ app.use(express.json());
 app.use(express.static("views"));
 
 app.use('/', function(req,res){
+    console.log("Serving page: ", path.join(__dirname+'/views/index.html'))
     res.sendFile(path.join(__dirname+'/views/index.html'));
-  });
-
+    console.log(res)
+});
 
 const externalUrl = process.env.RENDER_EXTERNAL_URL;
 const port = externalUrl && process.env.PORT ? parseInt(process.env.PORT) : 4080;
